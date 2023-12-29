464dfd3453e979362735ad49772815b6b1f5b4ee
itsmejatins <itsmejatin.s@gmail.com>
2023-12-29 11:52:20 +0530

    initial setup

---
+++ src/main/java/springjpa/detailed/DetailedApplication.java
@@ -0,0 +1,13 @@
+package springjpa.detailed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springjpa/detailed/DetailedApplicationTests.java
@@ -0,0 +1,13 @@
+package springjpa.detailed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
